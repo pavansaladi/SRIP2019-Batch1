8fbc197eebf461e27a9cb6def5ae3ce231798ef9
pavansaladi <35804570+pavansaladi@users.noreply.github.com>
2019-05-19 21:46:20 +0530

    Add files via upload

--- gate.js
+++ gate.js
@@ -23,3 +23,30 @@ function myFunction2() {
        document.getElementById("myImg2").src="og.gif";
      }
 }
+function myFunction3() {
+    var location = parseInt(document.getElementById("list1").value);
+    var location1=location+parseInt(document.getElementById("list2").value);
+    if(location1==2)
+    {
+        document.getElementById("name").innerHTML="AND Gate";
+    }
+    else if(location1==3)
+    {
+        document.getElementById("name").innerHTML="OR Gate";
+    }
+}
+function myFunction4() {
+    var location = parseInt(document.getElementById("list1").value);
+    var location1=location+parseInt(document.getElementById("list2").value);
+    var a=parseInt(prompt("Enter a value:","your first input here"));
+    var b=parseInt(prompt("Enter b value:","your second input here"));
+    if(location1==2)
+    {
+        var output=(a&&b);
+    }
+    else if(location1==3)
+    {
+        var output=(a||b);
+    }
+    document.getElementById("output").innerHTML="output value is "+output;
+}
